0775a5500c1b83ee171d5ef03d87aad2dac24759
ashisharya65 <ashisharya65@outlook.com>
2022-09-17 19:12:11 +0100

    Upload

--- Build-AzureResources.ps1
+++ Build-AzureResources.ps1
@@ -38,10 +38,10 @@ az group create --name $ResourceGroup --location $Region
 az storage account create --name $StorageAccountName --resource-group $ResourceGroup --location $Region --sku $StorageAccountSku --kind StorageV2
 
 # Enable static website hosting
-az storage blob service-properties update --account-name 'arya65storageaccount' --static-website --index-document index.html
+az storage blob service-properties update --account-name $StorageAccountName --static-website --index-document index.html
 
 # Upload website folders from local path
-az storage blob upload-batch --source "D:\Azure-Resume-Challenge" --destination '$web' --account-name 'arya65storageaccount'
+az storage blob upload-batch --source $WebsiteFilePath --destination '$web' --account-name $StorageAccountName
 
 # Display website URL
-az storage account show -n 'arya65storageaccount' --resource-group 'azureresume-rg' --query "primaryEndpoints.web" --output tsv
+az storage account show -n $StorageAccountName --resource-group $ResourceGroup --query "primaryEndpoints.web" --output tsv
